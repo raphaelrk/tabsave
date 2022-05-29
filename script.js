5f3b3e6159d84a5927a7b4e1140cb12ea04df4d1
Raphael Rouvinov-Kats <raphael.rouvinov@gmail.com>
2022-05-29 18:04:28 -0400

    manifest v3 + much nicer code

--- script.js
+++ script.js
@@ -1,130 +1,75 @@
-window.addEventListener("load", () => {
+const textarea = document.querySelector("#textarea");
+const copyButton = document.querySelector("button");
+const navCurrBtn = document.querySelector("#nav-curr");
+const navAllBtn = document.querySelector("#nav-all");
 
-    const textarea = document.querySelector("#textarea");
-    const button = document.querySelector("button");
-    const navCurrBtn = document.querySelector("#nav-curr");
-    const navAllBtn = document.querySelector("#nav-all");
+const getWindows = async () => {
+    const windows = await chrome.windows.getAll({ populate: true });
+    const currId = (await chrome.windows.getCurrent()).id;
 
-    // https://stackoverflow.com/a/43439734/3273806
-    function unescapeHtml(unsafe) {
-        return unsafe
-            .replace(/&amp;/g, "&")
-            .replace(/&lt;/g, "<")
-            .replace(/&gt;/g, ">")
-            .replace(/&quot;/g, '"')
-            .replace(/&#039;/g, "'")
-    }
-    function escapeHtml(unsafe) {
-        return unsafe
-            .replace(/&/g, "&amp;")
-            .replace(/</g, "&lt;")
-            .replace(/>/g, "&gt;")
-            .replace(/"/g, "&quot;")
-            .replace(/'/g, "&#039;");
-    }
-
-    const asyncGetCurrentWindowId = () => {
-        return new Promise((resolve, reject) => {
-            chrome.tabs.query({ "currentWindow": true }, tabs => {
-                resolve(tabs[0].windowId);
-            });
-        });
-    };
-    const isIncognito = (windowId) => {
-        return new Promise((resolve, reject) => {
-            chrome.windows.get(windowId, {}, res => {
-                resolve(res.incognito);
-            })
-        });
-    };
-
-    const setText = async (onlyCurrentWindow) => {
-        const currentWindowId = await asyncGetCurrentWindowId();
-
-        chrome.tabs.query(onlyCurrentWindow ? { "currentWindow": onlyCurrentWindow } : {}, async tabs => {
-
-            const tabsByWindow = {};
-            tabs.forEach(tab => {
-                tabsByWindow[tab.windowId] = tabsByWindow[tab.windowId] || [];
-                tabsByWindow[tab.windowId].push(tab);
-            });
-
-            // fetch incognito status of each window
-            // (requires toggling on "Allow In Incognito")
-            const windowInfo = {};
-            await Promise.all(Object.values(tabsByWindow).map(async tabs => {
-                const windowId = tabs[0].windowId;
-                const incognito = await isIncognito(windowId);
-                windowInfo[windowId] = { incognito };
-            }));
-
-            // put current window on top
-            // put incognito on bottom (unless it's current window)
-            const windows = Object.entries(tabsByWindow);
-            windows.sort(([a_id, a_tabs], [b_id, b_tabs]) => {
-                if (a_id == currentWindowId) return -1;
-                if (b_id == currentWindowId) return 1;
-                if (windowInfo[a_id].incognito !== windowInfo[b_id].incognito) { // could be more efficient
-                    if (windowInfo[a_id].incognito) return 1;
-                    return -1;
-                }
-                return 0;
-            });
-
-            const html = windows.map(([windowId, tabs]) => {
-                const windowPreamble = (windowInfo[windowId].incognito) ? `(Incognito)<br>` : ``;
-                return windowPreamble + tabs.map(tab => (`
-                    ${tab.title}
-                    <br>
-                    <a href="${escapeHtml(tab.url)}">${escapeHtml(tab.url)}</a>
-                `)).join("<br>");
-            }).join("<br><br>")
-
-            textarea.innerHTML = html;
-        });
-    };
-
-    const selectText = (shouldCopy) => {
-        window.setTimeout(function() {
-            var sel, range;
-            if (window.getSelection && document.createRange) {
-                range = document.createRange();
-                range.selectNodeContents(textarea);
-                sel = window.getSelection();
-                sel.removeAllRanges();
-                sel.addRange(range);
-            } else if (document.body.createTextRange) {
-                range = document.body.createTextRange();
-                range.moveToElementText(textarea);
-                range.select();
-            }
-            if (shouldCopy) {
-                document.execCommand("copy");
-                button.innerText = "Copied!";
-                setTimeout(() => {
-                    if (button.innerText.toLowerCase() === "copied!") button.innerText = "Copy";
-                }, 1000);
-            }
-        }, 1);
-    }
-
-    const setMode = async (onlyCurrentWindow) => {
-        navCurrBtn.style.fontWeight = onlyCurrentWindow ? "700" : "400";
-        navAllBtn.style.fontWeight = onlyCurrentWindow ? "400" : "700";
-        await setText(onlyCurrentWindow);
-        setTimeout(() => selectText(false), 100);
-    };
-
-    setMode(false);
-
-    button.addEventListener("click", () => {
-        selectText(true);
+    // put current window on top
+    // put incognito windows on bottom (only relevant if "Allow In Incognito" toggled on in settings)
+    windows.sort((a, b) => {
+        if (a.id == currId) return -1;
+        if (b.id == currId) return 1;
+        if (a.incognito && !b.incognito) return 1;
+        if (b.incognito && !a.incognito) return -1;
+        return 0;
     });
-    navCurrBtn.addEventListener("click", () => {
-        setMode(true);
-    });
-    navAllBtn.addEventListener("click", () => {
-        setMode(false);
-    });
-    
-});
+
+    return windows
+};
+
+const createHTML = async (allWindows) => {
+    let windows = await getWindows();
+    if (!allWindows) windows = [windows[0]];
+    return windows.map(w => `
+        ${w.incognito ? `(Incognito)<br>` : ``}
+        ${w.tabs.map(tab => `
+            ${tab.title}
+            <br>
+            <a href="${encodeURI(tab.url)}">${encodeURI(tab.url)}</a>
+        `).join("<br>")}
+    `).join("<br><br>");
+}
+
+const selectText = () => {
+    var sel, range;
+    if (window.getSelection && document.createRange) {
+        range = document.createRange();
+        range.selectNodeContents(textarea);
+        sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+    } else if (document.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(textarea);
+        range.select();
+    }
+}
+
+const copyText = () => {
+    selectText();
+    document.execCommand("copy");
+    copyButton.innerText = "Copied!";
+    setTimeout(() => {
+        if (copyButton.innerText.toLowerCase() === "copied!") copyButton.innerText = "Copy";
+    }, 1000);
+};
+
+const showContent = async (contentType) => {
+    navAllBtn.style.fontWeight = (contentType === 'ALL' ? "700" : "400");
+    navCurrBtn.style.fontWeight = (contentType === 'CURR' ? "700" : "400");
+    textarea.innerHTML = await createHTML(contentType === 'ALL');
+    selectText();
+};
+
+// load initial popup content. show all by default.
+showContent('ALL');
+
+// copy button pressed
+copyButton.addEventListener("click", () => copyText());
+
+// "all windows" or "current window" pressed
+navAllBtn.addEventListener("click", () => showContent('ALL'));
+navCurrBtn.addEventListener("click", () => showContent('CURR'));
